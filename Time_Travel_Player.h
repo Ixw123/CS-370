4b7516afe8c388f0b4316c69370999c485140820
Micah Church <micah.church.a@gmail.com>
2017-12-10 01:38:30 -0800

    Add files via upload

---
+++ Time_Travel_Player.h
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Time_Travel_Player.h
+ * Author: T420
+ *
+ * Created on November 7, 2017, 11:46 PM
+ */
+
+#ifndef TIME_TRAVEL_PLAYER_H
+#define TIME_TRAVEL_PLAYER_H
+
+void Player_move( SDL_Renderer* );
+
+
+#endif /* TIME_TRAVEL_PLAYER_H */
+
